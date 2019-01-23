edb60ee9c9072c72efb095365fda1044d1fbd2b3
marichka4345 <marichka4345@gmail.com>
2019-01-23 18:59:04 +0200

    [Draft] Implement form submit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component, Fragment } from 'react';
 import JssProvider from 'react-jss/lib/JssProvider';
 import { create } from 'jss';
 import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 import {TestForm} from './components/form/form';
 
@@ -19,11 +20,25 @@ export default class App extends Component {
 
     form = React.createRef();
 
+    submit = e => {
+        e.preventDefault();
+
+        console.log(this.form.current.getModel());
+    };
+
     render() {
         return (
           <JssProvider jss={jss} generateClassName={generateClassName}>
               <Fragment>
                   <TestForm ref={this.form} />
+
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={this.submit}
+                  >
+                      Save
+                  </Button>
               </Fragment>
           </JssProvider>
         );
